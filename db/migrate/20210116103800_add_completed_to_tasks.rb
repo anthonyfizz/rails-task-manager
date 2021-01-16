727ae768a239029e0fac78671011f64248b5f75e
anthony dupuis <anthonydupuis95@yahoo.fr>
2021-01-16 16:40:14 +0100

    exercic done

--- db/migrate/20210116103800_add_completed_to_tasks.rb
+++ db/migrate/20210116103800_add_completed_to_tasks.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class AddCompletedToTasks < ActiveRecord::Migration[6.0]
   def change
-    add_column :tasks, :completed, :boolean, default: false
+    change_column :tasks, :completed, :boolean, default: false
   end
 end
